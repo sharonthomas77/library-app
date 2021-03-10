d188ec51bd38869baee5762b91c07e6627d0d343
sharonthomas77 <mca547@rajagiri.edu>
2021-03-10 15:16:26 +0530

    navigation

--- content.php
+++ content.php
@@ -9,6 +9,28 @@
     <title>Content</title>
 </head>
 <body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Real Madrid</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="register.html">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="content.html">Content</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="index.html">Index</a>
+              </li>
+             
+            </ul>
+          </div>
+        </div>
+      </nav>
     <div class="container">
         <div class="row">
             <div class="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex excepturi impedit modi. Laborum officia praesentium vitae magnam consequatur eum nam rem neque, iusto, ratione, dolores aliquid mollitia necessitatibus facere facilis.</div>
@@ -16,5 +38,6 @@
             <div class="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet laborum impedit exercitationem laudantium quos hic sint perspiciatis nulla, voluptatem quibusdam tenetur, esse nemo numquam quod aut necessitatibus error ratione et.</div>
         </div>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>   
 </body>
 </html>
---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Library</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Real Madrid</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="register.html">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="content.html">Content</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="index.html">Index</a>
+              </li>
+             
+            </ul>
+          </div>
+        </div>
+      </nav>
+<div class="container">
+    <div class="row">
+        <div class="col col-12 col-sm-6 col-md-6 col-lg-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate expedita explicabo, debitis dolore nemo nulla quis alias necessitatibus assumenda fuga? Consectetur fugiat quibusdam iure omnis voluptate? Placeat quam mollitia perspiciatis?</div>
+        <div class="col col-12 col-sm-6 col-md-6">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam modi ratione minus laborum suscipit ipsum aliquam reiciendis. Provident nemo veniam sunt in cupiditate ea nostrum corrupti eius. Optio, quisquam quam.</div>
+    </div>
+</div>   
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>   
+
+</body>
+</html>
