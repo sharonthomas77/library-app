c7f64d8a570f830cf3bd3bb331f763454456ea26
sharonthomas77 <mca547@rajagiri.edu>
2021-03-10 15:50:55 +0530

    new

--- index.php
+++ index.php
@@ -2,10 +2,11 @@
 <html lang="en">
 <head>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Library</title>
+    <title>Sign up</title>
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -17,26 +18,91 @@
           <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="register.html">Home</a>
+                <a class="nav-link active" aria-current="page" href="register.php">Home</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="content.html">Content</a>
+                <a class="nav-link" href="content.php">Content</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="index.html">Index</a>
+                <a class="nav-link" href="index.php">Index</a>
               </li>
              
             </ul>
           </div>
         </div>
       </nav>
-<div class="container">
-    <div class="row">
-        <div class="col col-12 col-sm-6 col-md-6 col-lg-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate expedita explicabo, debitis dolore nemo nulla quis alias necessitatibus assumenda fuga? Consectetur fugiat quibusdam iure omnis voluptate? Placeat quam mollitia perspiciatis?</div>
-        <div class="col col-12 col-sm-6 col-md-6">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam modi ratione minus laborum suscipit ipsum aliquam reiciendis. Provident nemo veniam sunt in cupiditate ea nostrum corrupti eius. Optio, quisquam quam.</div>
+    <div class="container">
+        <div class="row">
+            <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <img height="500px" src="https://s01.sgp1.cdn.digitaloceanspaces.com/article/144528-mflmxxuwjs-1594973848.jpg" class="d-block w-100" alt="...">
+                  </div>
+                  <div class="carousel-item">
+                    <img height="500px" src="https://www.realmadrid.com/cs/Satellite?blobcol=urldata&blobheader=image%2Fjpeg&blobkey=id&blobtable=MungoBlobs&blobwhere=1203416406308&ssbinary=true" class="d-block w-100" alt="...">
+                  </div>
+                  <div class="carousel-item">
+                    <img height="500px" src="https://dynaimage.cdn.cnn.com/cnn/c_fill,g_auto,w_1200,h_675,ar_16:9/https%3A%2F%2Fcdn.cnn.com%2Fcnnnext%2Fdam%2Fassets%2F200716222727-real-madrid-trophy.jpg" class="d-block w-100" alt="...">
+                  </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"  data-bs-slide="prev">
+                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"  data-bs-slide="next">
+                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Next</span>
+                </button>
+              </div>
+        </div>
+        <div class="row">
+            <div class="col col-12 col-sm-6 col-md-6 col-lg-8">
+                <table class="table table-borderless">
+                    <tr>
+                        <td>Name</td>
+                        <td><input type="text" class="form-control"></td>
+                    </tr>
+                    <tr>
+                        <td>Roll Number</td>
+                        <td><input type="text" class="form-control"></td>
+                    </tr>
+                    <tr>
+                        <td>Admission Number</td>
+                        <td><input type="text" class="form-control"></td>
+                    </tr>
+                    <tr>
+                        <td>Password</td>
+                        <td><input type="password" class="form-control"></td>
+                    </tr>
+                    <tr>
+                        <td>Confirm Password</td>
+                        <td><input type="password" class="form-control"></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td><button class="btn btn-primary">Register</button></td>
+                    </tr>
+                </table>
+            </div>
+            <div class="col col-12 col-sm-6 col-md-6 col-lg-4">
+                <table class="table table-borderless">
+                    <tr>
+                        <td>Username</td>
+                        <td><input type="text" class="form-control"></td>
+                    </tr>
+                    <tr>
+                        <td>Password</td>
+                        <td><input type="text" class="form-control"></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td><button class="btn btn-success">Login</button></td>
+                    </tr>
+                </table>
+                
+            </div>
+        </div>
     </div>
-</div>   
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>   
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>   
 </body>
 </html>
